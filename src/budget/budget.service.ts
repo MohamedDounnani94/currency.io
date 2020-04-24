959f26d30adeb423e668f412d419e4853c1095e6
Mohamed Dounnani <mohamed.dounnani@weroad.it>
2020-04-24 18:01:11 +0200

    Partial integration of tests

--- src/budget/budget.service.ts
+++ src/budget/budget.service.ts
@@ -11,23 +11,23 @@ export class BudgetService {
     private budgetsRepository: Repository<Budget>,
   ) {}
 
-  async create(human: IBudget): Promise<string> {
-    return 'ciao'
+  async create(human: IBudget): Promise<Budget> {
+    return this.budgetsRepository.save(human)
   }
   async findAll(): Promise <Budget[]> {
     return this.budgetsRepository.find()
   }
 
-  findOneById(id: number): Promise <Budget> {
+  findOneById(id: string): Promise <Budget> {
     return this.budgetsRepository.findOne(id)
   }
 
-  async deleteOneById(id: number): Promise<string> {
+  async deleteOneById(id: string): Promise<string> {
     await this.budgetsRepository.delete(id)
     return `resource: ${id} successfully deleted`
   }
 
-  async updateById(id: number, human): Promise<Budget> {
+  async updateById(id: string, human): Promise<Budget> {
     await this.budgetsRepository.update(id, human)
     return this.budgetsRepository.findOne(id)
   }
